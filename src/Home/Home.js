556775b731683d28528667ee33c38ca7b8f21684
rokib97 <hasanrokib1997@gmail.com>
2022-04-08 03:23:53 +0600

    added password strength validation

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,5 @@
-import { getAuth } from "firebase/auth";
-import React from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import app from "../firebase.init";
 import "./Home.css";
@@ -7,6 +7,43 @@ import img from "./images/register.e58071de.png";
 
 const auth = getAuth(app);
 const Home = () => {
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [error, setError] = useState("");
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+  const handlePassBlur = (event) => {
+    setPass(event.target.value);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+
+    if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(pass)) {
+      setError(
+        "Must contain at least one number and one uppercase and lowercase letter,one special character and at least 8 or more characters"
+      );
+      return;
+    }
+    if (form.checkValidity() === true) {
+      createUserWithEmailAndPassword(auth, email, pass)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
   return (
     <div className="container mt-5">
       <div className="row">
@@ -18,20 +55,37 @@ const Home = () => {
         <div className="col-lg-6">
           <div className="login-details p-5 mt-2">
             <h1 className="my-3 text-primary">WELCOME BACK</h1>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                  required
+                  onBlur={handleEmailBlur}
+                  type="email"
+                  placeholder="Enter email"
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid email.
+                </Form.Control.Feedback>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  required
+                  onBlur={handlePassBlur}
+                  type="password"
+                  placeholder="Password"
+                />
+                <p className="text-danger">{error}</p>
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid password.
+                </Form.Control.Feedback>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out" />
               </Form.Group>
               <Button variant="primary" type="submit">
-                Submit
+                Log in
               </Button>
             </Form>
           </div>
